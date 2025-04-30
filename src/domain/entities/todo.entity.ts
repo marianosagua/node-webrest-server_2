2ec271e9c6bee4a449e04b180081e3b9ee4a75df
Mariano Sagua <marianosagua4343@gmail.com>
2025-04-30 00:34:07 -0300

    Add detailed documentation for fromObject method in TodoEntity

--- src/domain/entities/todo.entity.ts
+++ src/domain/entities/todo.entity.ts
@@ -9,6 +9,13 @@ export class TodoEntity {
     return !!this.completedAt;
   }
 
+  /**
+   * Este método estático toma un objeto genérico y lo convierte en una instancia de TodoEntity.
+   * Valida que el objeto tenga las propiedades 'id' y 'text', lanzando un error si alguna falta.
+   * Si 'completedAt' está presente, intenta convertirlo a una fecha válida.
+   * Si la conversión falla, lanza un error.
+   * Finalmente, retorna una nueva instancia de TodoEntity con los valores proporcionados.
+   */
   public static fromObject(object: { [key: string]: any }): TodoEntity {
     const { id, text, completedAt } = object;
 
